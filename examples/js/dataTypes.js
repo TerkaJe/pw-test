829773684e1a2b53837f26e073be9c5b9da37b0f
jendr <jendrejasova@gmail.com>
2024-11-19 20:21:22 +0100

    basic types

---
+++ examples/js/dataTypes.js
@@ -0,0 +1,37 @@
+const string1 = "test1.....'zvyrazneny text'"
+const string2 = 'test2....."zvyrazneny text"'
+const string3 = `test3 ...${string1}`
+ 
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+
+const num1 = 1234
+const num2 = 1234.90
+const num3 = -123.30
+const num4 = -33_445_672_166_000.999_999_999_999
+
+console.log(num1, num2, num3)
+console.log(num4)
+
+const bool1 = true
+const bool2 = false
+
+const null1 = null
+
+const undefined1 = undefined
+
+const object1 = {
+    klic1: "string",
+    klic2: 123,
+    klic3: null
+}
+console.log(object1)
+
+const array2 = [
+    "string",
+    1234,
+    null,
+
+]
---
+++ examples/js/variables.js
@@ -0,0 +1,21 @@
+console.log("test")
+let auto
+function declareLet()
+{
+    console.log('...')
+
+    auto = "Mercedes"
+    console.log(auto)
+}
+
+declareLet()
+
+auto = 1234
+console.log(auto)
+
+const letadlo = "Cesna"
+console.log(letadlo)
+
+var kolo
+kolo = 123
+console.log(kolo)
